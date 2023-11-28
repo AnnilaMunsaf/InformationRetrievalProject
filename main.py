1ad9c41d9f72c7b53fd677dbcf6e352685191442
Annila Munsaf <annilamunsaf2812@gmail.com>
2023-11-28 16:28:50 +0100

    Project Initial Setup

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import pandas as pd
+import spacy as sp
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import linear_kernel
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    movies_df = pd.read_csv('./wiki_movie_plots_deduped.csv',
+                           usecols=['Release Year', 'Title', 'Plot', 'Genre'])
+
+    movies_df.info()
+
+
+
+
+
+
+
